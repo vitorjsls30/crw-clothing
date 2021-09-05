2e4d89ed5639fccd21257065ce3b94e4bcd7e065
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2021-09-04 21:40:34 -0300

    crw-05 - Added user.selector to extract the user info from the reducer / Added the createStructuredSelector fn into the solution mapStateToProps usage

--- src/components/cart-icon/cart-icon.compoment.js
+++ src/components/cart-icon/cart-icon.compoment.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -15,12 +16,12 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
 });
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state),
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(
