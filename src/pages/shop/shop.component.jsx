e864213498a232dbf4ac13d2c44880e2496d1ba2
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2021-10-05 12:49:40 -0300

    crw-05 -Adding shop reducer and it's selectors into our root reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
 
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollections } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const ShopPage  = ({ collections }) => (
+  <div>
+    { collections.map(({id, ...otherCollectionProps}) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      )) }
+  </div>
+)
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+})
 
-  render() {
-    const { collections } = this.state;
-
-    return (
-    <div>
-      {
-        collections.map(({id, ...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))
-      }
-    </div>)
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
