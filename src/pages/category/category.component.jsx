076e1993361016bc272b13ae9cbcb03c0e75d451
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2021-10-06 11:25:46 -0300

    crw-05 - Adding CategoryPage component and the related Shop's page routing

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './category.styles.scss';
+
+const CategoryPage = ({ match }) => {
+  console.log('MATCH INSIDE IS', match);
+  return (
+    <div>CATEGORY PAGE!!</div>
+  )
+};
+
+export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../category/category.component';
 
-const ShopPage  = ({ collections }) => (
-  <div>
-    <CollectionsOverview />
-  </div>
-)
+const ShopPage = ({ match }) => {
+  console.log('MATCH', match);
+  return (
+    <div>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+    </div>
+  );
+}
 
 export default ShopPage;
