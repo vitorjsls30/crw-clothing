e82ec0592d6ea69660b3fd85e62b6fc97d877a02
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2021-10-14 20:09:40 -0300

    crw-09 - Adding WithSpinner HOC

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+
+const WithSpinner = WrapperComponent => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : 
+    <WrapperComponent {...otherProps} />
+  };
+  return Spinner;
+};
+
+export default WithSpinner;
