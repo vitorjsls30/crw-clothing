0c367cfc47b1a1268bb64a6296e9ecfd5f7d6c18
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2021-10-27 23:35:21 -0300

    crw-11 - Starting to integrate redux-saga into the project

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga' ;
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger, thunk];
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware];
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 const persistor = persistStore(store);
 
