298a08185c90ffcd2e19b70a4e526a2fde2f09eb
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2021-10-28 22:38:46 -0300

    crw-11 - Implementing fetchCollectionsAsync behavior using put saga effect

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,23 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { 
+  fetchCollectionsSuccess, 
+  fetchCollectionsFailure 
+} from './shop.actions';
 
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('yo! async fired!');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch(err) {
+    yield put(fetchCollectionsFailure(err.message))
+  }
 }
 
 export function* fetchCollectionsStart() {
