38d469366ea3d108502a3cb961438246356def20
vitor.sacramento <vjsacramento@TPZ-3J79CD3.MIDGARD.LOCAL>
2022-01-04 22:53:43 -0300

    crw-12 - adding useHistory and useSelector in cart-dropdown component / adding useParams usage into collection component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
@@ -7,8 +8,11 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = () => {
+  const { collectionId } = useParams();
+  const collection = useSelector(selectCollection(collectionId));
   const { title, items } = collection;
+
   return (
     <div className="collection-page">
       <h2>{ title }</h2>
@@ -23,8 +27,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
